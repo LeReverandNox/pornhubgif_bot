23cbb9890c92b30da57aded07ec87dfa799bc50a
lereverandnox <lereverandnox@gmail.com>
2018-03-29 00:10:03 +0200

    Begin work on PornHubService

--- src/services/PornHubService.py
+++ src/services/PornHubService.py
@@ -1,3 +1,9 @@
+import requests as r
+
 class PornHubService(object):
     def __init__(self):
-        pass
+        self._url = "https://pornhub.com"
+
+    def search_gifs(self, query):
+        print('On va rechercher des gifs pour {}'.format(query))
+        # Endpoint GET /gifs/search?search={}&page={}
