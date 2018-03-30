ddb321f9d4e463d632cae8215a515985347529e3
lereverandnox <lereverandnox@gmail.com>
2018-03-30 04:24:20 +0200

    Change gunicorn loglevel to info in prod

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $ENVIRONMENT == "prod" ]; then
-    exec gunicorn -b 0.0.0.0:8080 -w 3 -k gevent --log-file - --log-level debug --access-logfile - server:app
+    exec gunicorn -b 0.0.0.0:8080 -w 3 -k gevent --log-file - --log-level info --access-logfile - server:app
 elif [ $ENVIRONMENT == "dev" ]; then
     exec gunicorn -b 0.0.0.0:8080 -w 3 -k gevent --log-file - --log-level debug --access-logfile - server:app
 fi
